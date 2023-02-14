d442c94b0bce01ac5e59642a7d8f88a97dedeca8
matthcol <matthias.colin@gmail.com>
2023-02-14 10:37:49 +0100

    photo move ok

---
+++ photo_move.py
@@ -0,0 +1,51 @@
+import sys
+from pathlib import Path
+import re
+
+def checkIsDirectory(dirPath: Path):
+    if not dirPath.is_dir():
+        print(dirPath, "is not a directory")
+        sys.exit(2)    
+
+# read args
+if len(sys.argv) != 3:
+    print("Error: wrong number of arguments")
+    print("Usage: python photo_move.py srcdirc destdir")
+    sys.exit(1)
+
+srcdirPath = Path(sys.argv[1])
+destdirPath = Path(sys.argv[2])
+
+print(f"Will scan {srcdirPath} for photos to store in {destdirPath}")
+checkIsDirectory(srcdirPath)
+checkIsDirectory(destdirPath)
+
+for photoPath in srcdirPath.glob('*.jpg'):
+    # print(photoPath)
+    # print(photoPath.name)
+    if not re.match(r"IMG_[0-9]{8}", photoPath.name):
+        print("Warning: wrong photo name format (skip)", photoPath.name)
+        continue
+    dateTag = photoPath.name[4:12]
+    photoFolderPath = destdirPath / dateTag
+    photoFolderPath.mkdir(exist_ok=True)
+    # print("Move",photoPath, "to", photoFolderPath / photoPath.name)
+    photoPath.rename(photoFolderPath / photoPath.name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
